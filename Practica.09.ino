96660752d4d56b23f939a517bd54f49a0690dadb
DavidCespon <153514379+DavidCespon@users.noreply.github.com>
2023-12-11 13:13:39 +0100

    Update Practica.09.ino

--- Practica.09.ino
+++ Practica.09.ino
@@ -4,17 +4,14 @@ void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(13, HIGH);
-  delay(10000); // Wait for 10000 millisecond(s)
-  digitalWrite(12, HIGH);
-  digitalWrite(13, LOW);
-  delay(4000); // Wait for 4000 millisecond(s)
+  delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
 }
